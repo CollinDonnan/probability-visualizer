624c6d509b5fdcc1cff9172a50b9eaabfe417a64
Collin Donnan <collin@cd.cd>
2025-11-12 15:33:26 -0500

    Cleaing up routes

--- app.py
+++ app.py
@@ -7,35 +7,52 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-
     return render_template('index.html')
 
-@app.route('/<distro>/<float:n>/<int:trials>', methods=['GET', 'POST'])
+@app.route('/binomial/<int:trials>', methods=['GET', 'POST'])
 def stats(distro, n, trials):
     n = round(n,5)
-    try:
-        if distro == "geometric":
-            dist = geometric_distribution(trials, n)
-            return jsonify({"pmf": dist.pmf(), "mean": dist.mean(), "variance": dist.variance(), "stddev": dist.stddev(), "mgf": dist.mgf(), "cdf": dist.cdf()})
-    
-        if distro == "binomial":
-            dist  = binomial_distribution(trials, n)
-            return jsonify({"pmf": dist.pmf(), "mean": dist.mean(), "variance": dist.variance(), "stddev": dist.stddev(), "mgf": dist.mgf(), "cdf": dist.cdf()})
-    
-        if distro == "bernoulli":
-            dist = bernoulli_distro.bernoulli_distribution(n)
-            return jsonify({"pmf": dist.pmf(), "mean": dist.mean(), "variance": dist.variance(), "stddev": dist.stddev(), "mgf": dist.mgf(), "cmf": dist.cdf()})
-    
-        elif distro == "integer":
+    try: 
+        if distro == "integer":
             idx = int(trials)
             dist = integer_distrobution.integer_distribution(idx)
             return jsonify({"pmf": dist.pmf(), "mean": dist.mean(), "variance": dist.variance(), "stddev": dist.stddev(), "cdf": dist.cdf(), "mgf": dist.mgf()})
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+@app.route('/geometric/<float:n>/<int:trials>', methods=['GET', 'POST'])
+def stats(distro, n, trials):
+    n = round(n,5)
+    try:
+        if distro == "geometric":
+            dist = geometric_distribution(trials, n)
+            return jsonify({"pmf": dist.pmf(), "mean": dist.mean(), "variance": dist.variance(), "stddev": dist.stddev(), "mgf": dist.mgf(), "cdf": dist.cdf()})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route('/binomial/<float:n>/<int:trials>', methods=['GET', 'POST'])
+def stats(distro, n, trials):
+    n = round(n,5)
+    try:
+
+        if distro == "binomial":
+            dist  = binomial_distribution(trials, n)
+            return jsonify({"pmf": dist.pmf(), "mean": dist.mean(), "variance": dist.variance(), "stddev": dist.stddev(), "mgf": dist.mgf(), "cdf": dist.cdf()})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+
+@app.route('/bernoulli/<float:n>', methods=['GET', 'POST'])
+def stats(distro, n, trials):
+    n = round(n,5)
+    try:
+        if distro == "bernoulli":
+            dist = bernoulli_distro.bernoulli_distribution(n)
+            return jsonify({"pmf": dist.pmf(), "mean": dist.mean(), "variance": dist.variance(), "stddev": dist.stddev(), "mgf": dist.mgf(), "cmf": dist.cdf()})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
 
 
-    #distro doesnt match any implemented.
-    return jsonify({"error": "unknown distribution"}), 404
 if __name__ == "__main__":
     app.run(debug=True)
